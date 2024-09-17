c5ebd269dc00c0da473b42ea3dc5e753c2f028b5
maccollege <30079306@lcwc.ac.uk>
2024-09-17 18:34:36 +0000

    Finished work.

--- task.py
+++ task.py
@@ -1,7 +1,10 @@
 def task_1(): # Lists
 
     origional_list = ["Geoff", "Jeff", "Jeffrey"]
-
+    name = input("What name should be added to the list?")
+    origional_list.insert(0, name)
+    origional_list.remove("Jeff")
+    new_list = origional_list.copy()
     return new_list
 
 
@@ -9,24 +12,41 @@ def task_2(): # Dictionaries
 
     keys = ("name", "age", "profession")
     values = ("Geoff", 35, "technician")
-
+    person = dict(zip(keys, values))
+    car = {
+    "make": "Ford",
+    "model": "Focus",
+    "engine": 1.6,
+    "colour": "blue"
+    }
+    person.update({"car": car})
     return person
 
 
 def task_3(): # Tuples
     student_1 = ("Geoff", "Maths", 80)
-    student_2 = ()
-
+    name = input("What's student 2's name?")
+    subject = input("Their subject?")
+    score = int(input("Their score?"))
+    student_2 = (name, subject, score)
+    student_2 = tuple(student_2)
+    students = student_1 + student_2
     return students
 
 
 def task_4(): # Sets
     fruits_1 = {"apple", "banana", "cherry", "grape", "mango", "pineapple", "papaya","sprite", "orange", "lemon", "strawberry"}
     fruits_2 = {"raspberry", "banana", "cherry", "grape", "mango", "blueberry", "papaya", "melon", "lemon", "steak"}
+    fruits_1.discard("sprite")
+    fruits_2.discard("steak")
 
-    duplicate_fruits = None # This should be a tuple containing all the fruits in both tuples
-    individual_fruits = None # This should be a tuple containing only the individual fruits
-
+    duplicate_fruits = fruits_1.intersection(fruits_2)
+    individual_fruits = fruits_1.symmetric_difference(fruits_2)
 
     return [duplicate_fruits, individual_fruits] # Note - functions can only return one data item - so both tuples
                                                  # are contained inside a single list
+
+print(task_1())
+print(task_2())
+print(task_3())
+print(task_4())
